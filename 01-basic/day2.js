4ec4b3df536d7159538015b23640c8eeb5035fab
AzizurRehman-0125 <144048618+AzizurRehman-0125@users.noreply.github.com>
2023-11-21 01:52:51 +0000

    variable types and data types

---
+++ 01-basic/day2.js
@@ -0,0 +1,12 @@
+const number1Id=1234;
+let numId2="user name";
+var num3="password";
+
+console.table([number1Id,numId2,num3])
+
+//null =>standalone value
+//undefine=>value assign he ni hovi ha 
+//symbol=>unique
+
+///null tpype is object
+////undefine is undefine
